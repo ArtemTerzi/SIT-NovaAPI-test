f3579f44f230fed42072302468c967c7d1465364
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-08-03 16:42:02 +0300

    UPDATE: Default setting for a test project

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename="/react-template">
+    <BrowserRouter basename="/SIT-NovaAPI-test">
       <App />
     </BrowserRouter>
   </React.StrictMode>
