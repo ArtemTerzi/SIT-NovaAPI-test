f147bccfc6dc08c16be8374fbb3c4caf440bfb8d
Artem Terzi <terzi.artem.yuriyovych@gmail.com>
2023-08-08 13:49:51 +0300

    ADDED: GetInfoByClick on History Item

--- src/components/HistoryList/HistoryList.jsx
+++ src/components/HistoryList/HistoryList.jsx
@@ -1,21 +1,29 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { selectSearchHistoryList } from "redux/tracking/selectors";
 import {
   Container,
   SearchesList,
   SearchesListItem,
 } from "./HistoryList.styled";
+import { getDeliveryStatus } from "redux/tracking/operations";
 
 const HistoryList = () => {
   const searchHistoryList = useSelector(selectSearchHistoryList);
   const reversedList = [...searchHistoryList].reverse();
+  const dispatch = useDispatch();
+
+  const handleClick = (value) => {
+    dispatch(getDeliveryStatus(value));
+  };
 
   return (
     <Container>
       <h2>Search history</h2>
       <SearchesList>
         {reversedList.map(({ createdAt, Number }) => (
-          <SearchesListItem key={createdAt}>{Number}</SearchesListItem>
+          <SearchesListItem key={createdAt} onClick={() => handleClick(Number)}>
+            {Number}
+          </SearchesListItem>
         ))}
       </SearchesList>
     </Container>
